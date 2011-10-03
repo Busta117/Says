64f04a96d34ba800fca824edff3fa7003d72deca
Santiago Bustamante <busta117@gmail.com>
2011-10-03 09:10:39 -0500

    first commit

---
+++ Says/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Says
+//
+//  Created by Santiago on 3/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Says/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Says
+//
+//  Created by Santiago on 3/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
