5106dc53a55978d446cc4b4d71ba3f954175ea16
Santiago Bustamante <busta117@gmail.com>
2011-10-20 23:51:39 -0500

    eliminando lo que barba hizo del juego

--- Says/GameScene.m
+++ Says/GameScene.m
@@ -20,31 +20,6 @@
 -(id)init {
     if ((self = [super init])) {  
         
-        table = [[Table alloc] init];
-        
-        //Menu initialization
-        CCMenu* menu = [CCMenu menuWithItems: nil];
-        menu.position = CGPointMake([[CCDirector sharedDirector] winSize].width/2, [[CCDirector sharedDirector] winSize].height/2);
-
-        //Calculation of position for each cell
-        NSArray* cells = [table cells];
-        
-        for (Cell* cell in cells) {
-            [menu addChild:cell];
-            
-            CCJumpBy *action = [CCJumpBy actionWithDuration:1 position:ccp([[CCDirector sharedDirector] winSize].width / 3, [[CCDirector sharedDirector] winSize].height/ 3) height:25 jumps:2];
-            
-            [cell runAction:[CCSequence actions:action, [action reverse], nil]];
-            
-            //[cell runAction:[CCRotateBy actionWithDuration:1 angle:360]];
-        }
-        
-        //TO-DO: cambiarlo, debe ser dinamico segun el numero de celdas que tenga cells
-        [menu alignItemsInColumns:[NSNumber numberWithUnsignedInt:3],
-         [NSNumber numberWithUnsignedInt:3],
-         [NSNumber numberWithUnsignedInt:1],nil];
-        
-        [self addChild:menu];
     }
     
     return self;
